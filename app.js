ad878f4d7898b000d07f1360bfb9882b25afd19a
AVER343 <ankushvermagb@gmail.com>
2020-06-11 11:20:40 +0530

    comit12

--- app.js
+++ app.js
@@ -14,11 +14,14 @@ app.use(bodyParser.json())
 app.use(admin)
 app.use(passenger)
 app.use(express.static('client/build'));
-app.use(function(req, res) {
-	res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+if(process.env.NODE_ENV=='production')
+{
+    app.use(express.static('client/build'))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+}
+
+
 
 app.listen(PORT,()=>console.log('LISTENING AT 5000'))
