a2b20d1b5bd13f3a335b6d634d2e4663b190d4e2
AVER343 <ankushvermagb@gmail.com>
2020-06-11 11:51:40 +0530

    coms

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -3,6 +3,6 @@ import { Link } from 'react-router-dom'
 import * as logo from '../../assets/logo.png'
 const Header =({username})=>{
     return(<div><img src={logo}/>
-    {username=='admin'?"Sign OUT":"SignIn"}</div>)
+    {username=='admin'?"Sign OUT":<Link to='/admin'>"SignIn"</Link>}</div>)
 }
 export default Header
