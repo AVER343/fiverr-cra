79f221fec0289de26f80943c7ac425ab84e298cb
AVER343 <ankushvermagb@gmail.com>
2020-06-11 12:36:16 +0530

    more

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import * as logo from '../../assets/logo.png'
-import React from 'react';
 import {
   HeaderContainer,
   LogoContainer,
